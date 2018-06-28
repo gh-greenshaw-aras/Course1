92ccfef0d09fef4ef197b9273da751a0e1d5efb1
gh-greenshaw-aras <40636142+gh-greenshaw-aras@users.noreply.github.com>
2018-06-28 22:23:08 +0100

    Create listCountries.py

---
+++ listCountries.py
@@ -0,0 +1,18 @@
+# listCountries.py
+
+from artiom import *
+from christopher import *
+from mayurapiriyan import *
+from noor import *
+from ruth import *
+from simon import *
+
+print('Available countries are:\n' + getListOfCountries())
+
+print('The capital city of Nepal is ' + getCapitalCity('Nepal'))
+print('The currency in Cambodia is the ' + getCurrency('cambodia'))
+print('The area in square kilometres of Egypt is ' + getArea('Egypt'))
+print('The population of The Philippines is ' + getPopulation('Philippines'))
+print('The dialing code for Bulgaria is ' + getDialingCode('Bulgaria'))
+#print('The population density of The Philippines is ' + getPopulationDensity('Philippines') + ' per square kilometre')
+
